db65534a732d929d879928ec728a0258ad549cbd
Jeffrey McKinley <mckinley.jeffrey@gmail.com>
2022-02-06 12:00:22 -0700

    updating package to template

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,99 @@
 module.exports = {
-  siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+    siteMetadata: {
+        siteUrl: "https://www.yourdomain.tld",
+        title: "Inbio - Personal Portfolio React Template",
+        description: "Awesome Gatsby template",
+        author: "Rainbow IT",
+        siteLanguage: "en",
+        image: "banner.jpg",
+        titleTemplate: "inbio",
+        twitterUsername: "@rainbowit",
+        getform_url:
+            "https://getform.io/f/7a6695a7-c8e3-442c-bc2f-d46d3b9a535e",
+        socials: [
+            {
+                id: 1,
+                title: "facebook",
+                path: "https://facebook.com",
+                icon: "Facebook",
+            },
+            {
+                id: 2,
+                title: "instagram",
+                path: "https://instagram.com",
+                icon: "Instagram",
+            },
+            {
+                id: 3,
+                title: "linkedin",
+                path: "https://linkedin.com",
+                icon: "Linkedin",
+            },
+        ],
+        contact: {
+            phone: "01234567890",
+            email: "admin@example.com",
+        },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
-  ],
-}
+    plugins: [
+        {
+            resolve: "gatsby-plugin-sass",
+            options: {
+                useResolveUrlLoader: {
+                    options: {
+                        sourceMap: true, //default is false
+                    },
+                },
+            },
+        },
+        "gatsby-plugin-image",
+        "gatsby-plugin-react-helmet",
+        "gatsby-plugin-sitemap",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        "gatsby-transformer-json",
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/assets/images`,
+                ignore: [`**/\.*`],
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `data`,
+                path: `${__dirname}/src/data`,
+                ignore: [`**/\.*`],
+            },
+        },
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 1200,
+                        },
+                    },
+                    "gatsby-remark-reading-time",
+                ],
+            },
+        },
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: "Inbio",
+                short_name: "inbio",
+                theme_color: "#ff014f",
+                background_color: "#ffffff",
+                display: "standalone",
+                scope: "/",
+                start_url: "/",
+                icon: "src/assets/images/favicon.png",
+            },
+        },
+    ],
+};
