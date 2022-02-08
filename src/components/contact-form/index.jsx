b8f85aa7986c38058b0a1b0d1f16c91827b659da
Jenny Riley <jenny.p.riley@gmail.com>
2022-02-08 11:17:29 -0700

    Fixed contact form styling bugs and improved validation for invalid email

--- src/components/contact-form/index.jsx
+++ src/components/contact-form/index.jsx
@@ -107,7 +107,8 @@ const ContactForm = ({ className, url }) => {
                                     required: "Email is required",
                                     pattern: {
                                         value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                                        message: "invalid email address",
+                                        message:
+                                            "Email must follow this format: name@email.com",
                                     },
                                 })}
                             />
